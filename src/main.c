116f8b57aa179fceac2ea8c786e7e39c3c1c9ab2
Nicolas Martin <nicolmar@c1r1p11.42lehavre.fr>
2025-04-24 11:02:08 +0200

    add mapfile type checking

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "cub3d.h"
 
-int check_arg(int argc)
+int	check_arg(int argc)
 {
 	if (argc > 2)
 		return (ft_putendl_fd(ERR_TM_ARG, 2), 1);
@@ -9,11 +9,11 @@ int check_arg(int argc)
 	return (0);
 }
 
-int main(int argc, char const *argv[])
+int	main(int argc, char const *argv[])
 {
-	if(check_arg(argc) != 0)
+	if (check_arg(argc) != 0)
 		return (1);
-	(void)argv;
-	printf("Hello world!");
-	return 0;
+	if (check_mapfile((char *)argv[1]) == 0)
+		ft_printf("YATAAAA!");
+	return (0);
 }
