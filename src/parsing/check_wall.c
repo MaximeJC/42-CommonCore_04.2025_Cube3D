8ecf2619a9ceb2c0ef3c7aecf2e71add83fda1ce
Nicolas Martin <nicolmar@c1r1p11.42lehavre.fr>
2025-04-28 13:37:43 +0200

    create function for check validity of map, wall and ground

--- src/parsing/check_wall.c
+++ src/parsing/check_wall.c
@@ -3,18 +3,20 @@
 char	**ft_strtab_dup(char **tab);
 void ft_print_tab(char **tab);
 void set_player_data(char c, t_data *data, int y, int x);
+int recursive_check(char **c_map, int pos_x, int pos_y);
 int	find_player_pose(char **c_map, t_data *data);
+int	map_error_type(char c);
 
 int	check_wall_and_ground(t_data *data)
 {
-	int		d_x;
-	int		d_y;
 	char	**c_map;
 
 	c_map = ft_strtab_dup(data->d_map->map);
 	if(!c_map)
 		return(error_handler(ERR_MALLOC, NULL, 0), 1);
 	if (find_player_pose(c_map, data) != 0)
+		return (ft_strtab_free(c_map), 1);
+	if (recursive_check(c_map, data->player->pos_y, data->player->pos_x) != 0)
 		return (ft_strtab_free(c_map), 1);
 	return (ft_strtab_free(c_map), 0);
 }
@@ -75,6 +77,46 @@ void	set_player_data(char c, t_data *data, int y, int x)
 	}
 }
 
+int recursive_check(char **c_map, int pos_y, int pos_x)
+{
+	int rslt;
+
+	ft_print_tab(c_map);
+	rslt = 0;
+
+	c_map[pos_y][pos_x] = '2';
+	if (rslt >= 0 && c_map[pos_y - 1][pos_x] == '0')
+		rslt = recursive_check(c_map, pos_y - 1, pos_x);
+	else if (rslt >= 0)
+		rslt = map_error_type(c_map[pos_y - 1][pos_x]);
+	if ( rslt >= 0 && c_map[pos_y + 1][pos_x] == '0')
+		rslt = recursive_check(c_map, pos_y + 1, pos_x);
+	else if (rslt >= 0)
+		rslt = map_error_type(c_map[pos_y + 1][pos_x]);
+	if ( rslt >= 0 && c_map[pos_y][pos_x - 1] == '0')
+		rslt = recursive_check(c_map, pos_y, pos_x - 1);
+	else if (rslt >= 0)
+		rslt = map_error_type(c_map[pos_y][pos_x - 1]);
+	if ( rslt >= 0 && c_map[pos_y][pos_x + 1] == '0')
+		rslt = recursive_check(c_map, pos_y, pos_x + 1);
+	else if (rslt >= 0)
+		rslt = map_error_type(c_map[pos_y][pos_x + 1]);
+	if (rslt == 1)
+		return (0);
+	else if (rslt == -1)
+		return (-1);
+	return (0);
+}
+
+int	map_error_type(char c)
+{
+	if (c == '1' || c == '2')
+		return (1);
+	if (c == '\0' || c == ' ')
+		return (error_handler(ERR_WALL, NULL, 0), -1);
+	else
+		return (error_handler(ERR_CHAR, NULL, 0), -1);
+}
 
 char	**ft_strtab_dup(char **tab)
 {
