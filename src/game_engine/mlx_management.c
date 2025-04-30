0cbac4e3a13422eb5a47649a166442cc536fb919
Maxime Gouraud <mgouraud@student.42lehavre.fr>
2025-04-30 11:44:47 +0200

    Correct mlx textures init img_size pointer

--- src/game_engine/mlx_management.c
+++ src/game_engine/mlx_management.c
@@ -2,6 +2,9 @@
 
 void	mlx_win_init(t_data *data)
 {
+	int	temp;
+
+	temp = 0;
 	data->mlx_ptr = mlx_init();
 	if (!data->mlx_ptr)
 		return (error_handler(ERR_MLX_INIT, data, 1));
@@ -10,13 +13,13 @@ void	mlx_win_init(t_data *data)
 	if (!data->mlx_win)
 		return (error_handler(ERR_WIN_INIT, data, 1));
 	data->d_map->no_text_mlx = MLX_IMG_INIT(data->mlx_ptr,
-		data->d_map->no_texture, IMG_SIZE, IMG_SIZE);
+		data->d_map->no_texture, &temp, &temp);
 	data->d_map->so_text_mlx = MLX_IMG_INIT(data->mlx_ptr,
-		data->d_map->so_texture, IMG_SIZE, IMG_SIZE);
+		data->d_map->so_texture, &temp, &temp);
 	data->d_map->ea_text_mlx = MLX_IMG_INIT(data->mlx_ptr,
-		data->d_map->ea_texture, IMG_SIZE, IMG_SIZE);
+		data->d_map->ea_texture, &temp, &temp);
 	data->d_map->we_text_mlx = MLX_IMG_INIT(data->mlx_ptr,
-		data->d_map->we_texture, IMG_SIZE, IMG_SIZE);
+		data->d_map->we_texture, &temp, &temp);
 	if (!data->d_map->no_text_mlx || !data->d_map->so_text_mlx
 		|| !data->d_map->ea_text_mlx || !data->d_map->we_text_mlx)
 		return (error_handler(ERR_IMG_INIT, data, 1));
