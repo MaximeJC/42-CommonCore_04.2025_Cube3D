36d1af28aaf7b7c45001e31fe4b54a7bfbc41a49
Maxime Gouraud <mgouraud@student.42lehavre.fr>
2025-05-06 13:31:33 +0200

    Fix ft_putendl() to error_handler()

--- src/struct/struct_player.c
+++ src/struct/struct_player.c
@@ -4,7 +4,7 @@ int	init_player(t_data *data)
 {
 	data->player = ft_calloc(1, sizeof(t_player));
 	if (data->player == NULL)
-		return (ft_putendl_fd(ERR_MALLOC, 2), 1);
+		return (error_handler(ERR_MALLOC, NULL, 0), 1);
 	data->player->pos_x = 0;
 	data->player->pos_y = 0;
 	data->player->dir_x = 0;
