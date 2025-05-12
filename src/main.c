b7053b234b25e459e8ba6d8753a07566e33914b2
Nicolas Martin <nicolmar@c1r1p11.42lehavre.fr>
2025-05-12 16:50:19 +0200

    hide mouse cursor

--- src/main.c
+++ src/main.c
@@ -23,6 +23,7 @@ int	main(int argc, char const *argv[])
 		return (clear_data(&data), 1);
 	init_raycast(&data);
 	mlx_win_init(&data);
+	mlx_mouse_hide(data.mlx_ptr, data.mlx_win);
 	mlx_hook(data.mlx_win, 6, 1L << 6, mouse_move, &data);
 	mlx_hook(data.mlx_win, 17, 0, close_mlx, &data);
 	mlx_hook(data.mlx_win, 2, 1L << 0, key_press, &data);
