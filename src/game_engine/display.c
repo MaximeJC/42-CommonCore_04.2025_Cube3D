437896a6c104686a0f7bf4078e2e4f80094f1f60
mgouraud <mgouraud@student.42lehavre.fr>
2025-05-13 00:15:37 +0200

    Bugfix : Minmap pixels should now be correct size

--- src/game_engine/display.c
+++ src/game_engine/display.c
@@ -49,11 +49,11 @@ void	minimap_drawing(t_data *data)
 	int	i;
 	int	j;
 
-	y = 9;
-	while (++y < data->d_map->height + 10)
+	y = -1;
+	while (++y < data->d_map->height)
 	{
-		x = 9;
-		while (++x < data->d_map->width + 10)
+		x = -1;
+		while (++x < data->d_map->width)
 		{
 			if (data->d_map->map[y][x] == ' ')
 				continue ;
@@ -74,11 +74,11 @@ static void	minimap_drawing_bis(t_data *data, int x, int y, int color)
 	int	j;
 
 	i = -1;
-	while (++i < 3)
+	while (++i < MINMAP_PIX_SIZE)
 	{
 		j = -1;
-		while (++j < 3)
-			ft_mlx_pixel_put(data, (x - 10) + i,
-				(y - 10) + j, color);
+		while (++j < MINMAP_PIX_SIZE)
+			ft_mlx_pixel_put(data, x * MINMAP_PIX_SIZE + 16 + i,
+				y * MINMAP_PIX_SIZE + 16 + j, color);
 	}
 }
