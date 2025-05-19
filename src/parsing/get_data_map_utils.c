692cbb82d6f285834813d756739057a716fd18f2
Nicolas Martin <nicolmar@c1r1p11.42lehavre.fr>
2025-05-19 11:33:53 +0200

    norm

--- src/parsing/get_data_map_utils.c
+++ src/parsing/get_data_map_utils.c
@@ -6,7 +6,7 @@
 /*   By: nicolmar <nicolmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 11:38:15 by nicolmar          #+#    #+#             */
-/*   Updated: 2025/05/13 15:26:00 by nicolmar         ###   ########.fr       */
+/*   Updated: 2025/05/19 11:33:36 by nicolmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	check_empty_line(char *line, int err)
 {
-	static int c_no = 0;
-	static int c_so = 0;
-	static int c_ea = 0;
-	static int c_we = 0;
+	static int	c_no = 0;
+	static int	c_so = 0;
+	static int	c_ea = 0;
+	static int	c_we = 0;
 
 	if (err != 0)
 		return (1);
@@ -30,7 +30,7 @@ int	check_empty_line(char *line, int err)
 	else if (ft_strncmp(line, "WE ", 3) == 0)
 		c_we++;
 	if (c_no > 1 || c_so > 1 || c_ea > 1 || c_we > 1)
-	   	return (error_handler(ERR_TEX_NB, NULL, 0), 1);
+		return (error_handler(ERR_TEX_NB, NULL, 0), 1);
 	if (ft_strncmp(line, "SO ", 3) == 0 || ft_strncmp(line, "NO ", 3) == 0
 		|| ft_strncmp(line, "EA ", 3) == 0 || ft_strncmp(line, "WE ", 3) == 0
 		|| ft_strncmp(line, "C ", 2) == 0 || ft_strncmp(line, "F ", 2) == 0
